6124d1a725f06bfd0bbc802bb02c96b912ede4a9
Richard <richard@tlgd.co.uk>
2022-09-05 11:07:34 +0100

    Typo

--- server/app.js
+++ server/app.js
@@ -3,5 +3,5 @@ const express = require('express');
 const app = express();
 
 app.listen(4000, () => {
-    console.log('Now listening for request on port 4000');
+    console.log('Now listening for requests on port 4000');
 })
