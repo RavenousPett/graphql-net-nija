026a6d684a08ca5c6b4227ce84f01e14180c43f3
Richard <richard@tlgd.co.uk>
2022-09-06 11:43:38 +0100

    Lists

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,14 +6,18 @@ const {
   GraphQLString,
   GraphQLID,
   GraphQLInt,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList
 } = graphql;
 
 // dummy data
 var books = [
   { name: 'The Ink Black Heart', genre: 'Crime Mystery', id: '1', authorId: '1'},
   { name: 'A Flicker in the Dark', genre: 'Crime Mystery', id: '2', authorId: '2'},
-  { name: 'Where the Crawdads Sing', genre: 'Fantasy', id: '3', authorId: '3'}
+  { name: 'Where the Crawdads Sing', genre: 'Fantasy', id: '3', authorId: '3'},
+  { name: 'The Paper Palace', genre: 'Romance', id: '4', authorId: '3'},
+  { name: 'Carrie Soto Is Back', genre: 'Romance', id: '5', authorId: '1'},
+  { name: 'The Rising Tide', genre: 'Crime Mystery', id: '6', authorId: '2'}
 ];
 
 var authors = [
@@ -42,7 +46,13 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args){
+        return _.filter(books, { authorId: parent.id })
+      }
+    }
   })
 });
 
