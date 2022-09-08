8f31e2086158e544a8ae436a7588799537135c5a
Richard <richard@tlgd.co.uk>
2022-09-08 11:51:27 +0100

    Making queries from react, also add cors to the express server

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,4 +1,23 @@
+import { gql, useQuery } from '@apollo/client';
+
+const getBooksQuery = gql`
+  {
+    books{
+      name
+      id
+    }
+  }
+`
+
 function BookList() {
+  const { loading, error, data } = useQuery(getBooksQuery);
+
+  console.log(data);
+
+  if(loading) return <p>Loading....</p>
+
+  if(error) return <p>Ops! Something went wrong</p>
+
   return (
     <div>
       <ul id="book-list">
